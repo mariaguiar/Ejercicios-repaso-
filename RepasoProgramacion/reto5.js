b11613fab8e235623c6063bb46ab31dc24a27124
mariaguiar <65535353+mariaguiar@users.noreply.github.com>
2020-05-18 18:03:04 +0000

    Add files via upload

---
+++ RepasoProgramacion/reto5.js
@@ -0,0 +1,12 @@
+function arcoiris(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === "verde" || array[i] === "azul" || array[i] === "amarillo" || array[i] === "rojo"
+            || array[i] === "naranja" || array[i] === "morado") {
+            console.log("Si esta en el arcoiris");
+        }
+        else {
+            console.log("No esta en el arcoiris");
+        }
+    }
+}
+arcoiris(["verde", "amarillo"]);
---
+++ RepasoProgramacion/reto9.js
@@ -0,0 +1,9 @@
+function parimpar(num) {
+    if (num % 2 == 0) {
+        console.log("El numero es par");
+    }
+    else {
+        console.log("El numero es impar");
+    }
+}
+parimpar(5);
