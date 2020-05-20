cc6b2ab57054d51d381bf0f0dbf623400fb06285
mariaguiar <65535353+mariaguiar@users.noreply.github.com>
2020-05-20 07:56:46 +0000

    Update RepasoFor.ts

--- RepasoProgramacion/RepasoFor.ts
+++ RepasoProgramacion/RepasoFor.ts
@@ -71,21 +71,6 @@ function  Par(Array){
 }
 
 
-// Reto 7
-
-function nombres(array)
-{
-   for(var i=0; i<array.length; i++) 
-   {
-      for(var j=0; j<array[i].length; j++) 
-      {
-      if (array[i][0]=== "M"){}
-      }
-      console.log(array[i],": Verdadero");
-   }
-}
-
-nombres(["Maria", "Marta"]);
 
 //Reto 8
 
