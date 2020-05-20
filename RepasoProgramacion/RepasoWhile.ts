b72cb4b9f358e49b94846b14034b9807bb4900a5
mariaguiar <65535353+mariaguiar@users.noreply.github.com>
2020-05-20 07:55:57 +0000

    Update RepasoWhile.ts

--- RepasoProgramacion/RepasoWhile.ts
+++ RepasoProgramacion/RepasoWhile.ts
@@ -9,3 +9,20 @@ while (i<=n1)
    console.log (i);
    i+=2;
 }
+
+//Reto 7
+
+function nombres(array)
+{
+ let i = 0;
+ let m = true;
+ while (i<array.length && m)
+ {
+  if (!array[i].startsWith("M"))
+  {m = false}
+  i++;
+ }
+ return m;
+}
+
+console.log(nombres(["Maria", "Pepe"]));
